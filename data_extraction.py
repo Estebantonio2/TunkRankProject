1cdca71097924956f7b46b154c9457a02c1841cd
esteban_terrones <esteban20001118@gmail.com>
2025-07-08 22:20:47 -0500

    ultimos cambios de tests

--- data_extraction.py
+++ data_extraction.py
@@ -1,11 +1,11 @@
-def build_adjacency_matrix(edge_file, n):
+def build_adjacency_matrix(edge_file, n=None):
     following = {}
     nodes = set()
     count = 0
 
     with open(edge_file, 'r') as f:
         for line in f:
-            if count >= n:
+            if n is not None and count >= n:
                 break
             edges = line.strip().split()
             if len(edges) == 2:
@@ -26,7 +26,7 @@ def build_adjacency_matrix(edge_file, n):
             j = node_to_idx[v]
             matrix[i][j] = 1
 
-    with open('matrix_tw.txt', 'w') as f:
+    with open('matrix3.txt', 'w') as f:
         f.write('matrix = [\n')
         for i, row in enumerate(matrix):
             row_str = ', '.join(str(x) for x in row)
@@ -37,4 +37,4 @@ def build_adjacency_matrix(edge_file, n):
         f.write(']\n')
     print('Matriz guardada en matrix_tw.txt con formato de lista de listas')
 
-build_adjacency_matrix('twitter_combined.txt', 1000)
+build_adjacency_matrix('twitter_combined.txt', 100000) # Numero de lineas a leer del archivo
--- tunkRankImplementation.py
+++ tunkRankImplementation.py
@@ -16,8 +16,7 @@ def heapify_down(arr, size, i):
 
 def build_max_heap(arr):
     n = len(arr)
-    # Comienza desde el último nodo no-hoja hasta la raíz
-    for i in range((n // 2) - 1, -1, -1):
+    for i in range(parent(n - 1), -1, -1):
         heapify_down(arr, n, i)
 
 def extract_max(arr, size):
@@ -30,7 +29,7 @@ def extract_max(arr, size):
     return max_val, size
 
 def top_k_tunkrank(T, k=3):
-    # Convertir el diccionario T en una lista de tuplas (score, user_id)
+    # Convertir el diccionario T en una lista de tuplas (score, id)
     heap_arr = [(score, u) for u, score in T.items()]
 
     # Construir max-heap
@@ -73,14 +72,14 @@ def adjacency_to_followers(adj):
 
 # Imprime los scores de los nodos en cada iteración
 def print_T_iteration(i, T_it):
-    print(f"Iteration {i+1}:")
+    print(f"Iteracion {i+1}:")
     for j in range(len(T_it)):
         print(f"\t{j}: {T_it[j]:.8f}")
         pass
 
 # Imprime los scores finales de los nodos
 def print_scores(T, names=None):
-    print("Final scores:")
+    print("Scores finales:")
     for u, score in T.items():
         if names is not None:
             u = names[u]
@@ -88,7 +87,7 @@ def print_scores(T, names=None):
     print()
 
 # Implementación del algoritmo TunkRank
-def tunkrank(adj, p=0.25, max_iter=100, tol=1e-6):
+def tunkrank(adj, p=0.25, max_iter=100, tol=1e-6, print_iterations=False):
     following = adjacency_to_following(adj)
     followers = adjacency_to_followers(adj)
 
@@ -107,60 +106,74 @@ def tunkrank(adj, p=0.25, max_iter=100, tol=1e-6):
             delta = max(delta, abs(T_new[x] - T[x]))
         T = T_new
 
-        # print_T_iteration(it, list(T.values()))
+        if print_iterations:
+            print_T_iteration(it, list(T.values()))
         
         if delta < tol:
-            print(f"Convergió en {it+1} iteraciones (δ={delta:.2e})")
+            print(f"Convergio en {it+1} iteraciones (δ={delta:.2e})")
             break
     else:
-        print(f"No convergió tras {max_iter} iteraciones (δ={delta:.2e})")
+        print(f"No convergio tras {max_iter} iteraciones (δ={delta:.2e})")
 
     return T
 
 def main():
     # Test 1
+    # Ejemplo de matriz de adyacencia simple y nombres de usuarios
+    print("==================== Test 1 ====================")
+    
     names1 = ["Alice", "Bob", "Carol", "Dave"]
     matrix1 = [ [0, 1, 0, 0],
                 [0, 0, 0, 0],
                 [0, 1, 0, 1],
                 [0, 0, 1, 0] ]
     
-    T1 = tunkrank(matrix1, p=0.2, max_iter=1000, tol=1e-8)
+    T1 = tunkrank(matrix1, p=0.2, max_iter=1000, tol=1e-8, print_iterations=True)
+    print("Numero de usuarios:", len(T1))
     print_scores(T1, names=names1)
     
     # Test 2
-    names2 = ['Juan', 'Mathias', 'Santiago', 'Gael', 'Gabriel']
-    matrix2 = [
-        [0, 1, 0, 0, 0],
-        [1, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 1, 0, 1],
-        [0, 1, 0, 1, 0],
-    ]
-    T2 = tunkrank(matrix2, p=0.4, max_iter=1000, tol=1e-8)
-    print_scores(T2, names=names2)
+    # Ejemplo de matriz de adyacencia de las primeras 100 lineas de las relaciones del dataset Social circles: Twitter
+    # https://snap.stanford.edu/data/ego-Twitter.html
+    print("\n==================== Test 2 ====================")
+    
+    with open('matrix2.txt', 'r') as f:
+        content = f.read()
+    local_vars = {}
+    exec(content, {}, local_vars)
+    matrix2 = local_vars['matrix']
+    
+    T2 = tunkrank(matrix2, p=0.3, max_iter=100, tol=1e-6)
+    print("Numero de usuarios:", len(T2))
+    # print_scores(T2)
 
     # Top 3 scores
+    # Extraer los 3 usuarios con mayor score
     top3 = top_k_tunkrank(T2, k=3)
-    print("Top 3 scores (user_id, score):")
-    for score, user_id in top3:
-        print(f"\t{names2[user_id]}: {score:.8f}")
+    print("Top 3 scores (id, score):")
+    for score, id in top3:
+        print(f"\t{id}: {score:.8f}")
 
     # Test 3
-    with open('matrix_tw.txt', 'r') as f:
+    # Ejemplo de matriz de adyacencia de las primeras 100000 lineas de las relaciones del dataset Social circles: Twitter
+    # https://snap.stanford.edu/data/ego-Twitter.html
+    print("\n==================== Test 3 ====================")
+    
+    with open('matrix3.txt', 'r') as f:
         content = f.read()
     local_vars = {}
     exec(content, {}, local_vars)
     matrix3 = local_vars['matrix']
 
-    T3 = tunkrank(matrix3, p=0.4, max_iter=1000, tol=1e-8)
+    T3 = tunkrank(matrix3, p=0.4, max_iter=100, tol=1e-5)
+    print("Numero de usuarios:", len(T3))
     # print_scores(T3)
     
     # Top 5 scores
+    # Extraer los 5 usuarios con mayor score
     top5 = top_k_tunkrank(T3, k=5)
     print("Top 5 scores (id, score):")
     for score, id in top5:
         print(f"\t{id}: {score:.8f}")
-
 
 main()
